a1c885ef96d18e2ca6cb4500b4c96384dbc3d8c7
lgcmotta <lgcmotta01@gmail.com>
2025-11-01 16:09:06 -0300

    fix: make subscription keys active by default

--- iac/modules/subscription_key/main.tf
+++ iac/modules/subscription_key/main.tf
@@ -18,6 +18,8 @@ resource "azurerm_api_management_subscription" "this" {
   resource_group_name = var.resource_group_name
   api_management_name = var.api_management_name
   display_name        = each.value.display_name
+  allow_tracing       = true
+  state               = "active"
 }
 
 output "subscriptions" {
