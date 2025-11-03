306221d3f4631986dbf9a57ba45d0df4b074910d
lgcmotta <lgcmotta01@gmail.com>
2025-11-03 00:49:35 -0300

    fix(web): pass transport when configuring hub connection

--- apps/src/azure-signalr-hubclient-web/src/lib/signalr.ts
+++ apps/src/azure-signalr-hubclient-web/src/lib/signalr.ts
@@ -31,7 +31,7 @@ class Connector {
         },
         accessTokenFactory: options.accessTokenFactory,
         skipNegotiation: options.skipNegotiation,
-        transport: HttpTransportType.WebSockets
+        transport: options.transport
       })
       .withAutomaticReconnect()
       .build();
